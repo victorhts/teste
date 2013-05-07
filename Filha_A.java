9d6ae7bbc7d939add758245eb29a9f57df4bc149
Gabriel Queiroz <gabriel.fatec@hotmail.com>
2013-05-07 08:06:52 -0300

    Teste
    Adicionada duas filhas ao projeto.

---
+++ Filha_A.java
@@ -0,0 +1,7 @@
+package Pai;
+
+public class Filha_A extends Pai {
+	public Filha_A(){
+		System.out.println("Construtor Filha A");
+	}
+}
---
+++ Filha_B.java
@@ -0,0 +1,7 @@
+package Pai;
+
+public class Filha_B extends Pai{
+	public Filha_B(){
+		System.out.println("Construtor Filha B");
+	}
+}
